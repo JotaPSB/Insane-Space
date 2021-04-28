fab6136615eb5135cc55bb814e5924a7a8a80fa5
JotaPSB <juan.sanchez.7e3@itb.cat>
2021-04-28 14:03:49 +0200

    Doors, KeyPad, Notification System

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public bool lockedByPassword;
+    public DialogueTrigger trigger;
+    public Animator anim;
+
+    public void OpenClose()
+    {
+        if (lockedByPassword)
+        {
+            trigger.TriggerDialogue();
+
+            Debug.Log("Nice");
+            return;
+        }
+
+        anim.SetBool("OpenDoor", true);
+    }
+
+}
